af6f6b7fcbec9d5b818c43a91fcf07525c08849c
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2025-01-14 23:50:39 +0530

    Add files via upload

---
+++ backend/gatwayserver/src/test/java/com/workdirecz/GatwayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workdirecz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatwayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
